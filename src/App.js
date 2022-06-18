dea7e05462dc2a6d352c3e123ffaf4a909744275
nadhir58 <benamornadhir58@gmail.com>
2022-06-18 10:54:43 +0200

    'second'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import photo from "./imageInSrc.jpg"
+import "./style.css"
+ function Home() {
+  return (
+    <div>
+      <img src={photo} alt="BigCo Inc. logo"/>
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+    <h1 className="title red">Nadhir Ben Amor</h1>
+    <br />
+    
+  </div>
+  <video width={320} height={240} controls="">
+    <source src="myVideo.mp4" type="video/mp4" />
+  </video>
+  <Home/>
+     
     </div>
   );
 }
